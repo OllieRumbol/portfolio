bd29dcffed8ff5419acd82a36098142b654fe950
OllieRumbol <otgb97@gmail.com>
2021-05-16 22:02:38 +0100

    small changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
-import '../style/Home.css'
+import '../style/Home.css';
 
 export default function Home() {
 
@@ -8,18 +8,16 @@ export default function Home() {
     <div className='p-4'>
       <Jumbotron fluid>
         <Container className='header'>
-          <h1 className='title'>Oliver BOURNE Portfolio</h1>
+          <h1 className='title'>Oliver BOURNE</h1>
+          <h1 className='subTitle'>Software Developer</h1>
           <hr className='green' />
           <h5 className='p-2'>
-            .Net Developer with a passion for creating well structured and reliable solutions using the Microsoft stack of technologies. 
+            .Net Developer with a passion for creating well structured and reliable solutions using the Microsoft stack of technologies.
             I can't wait to learn new tools, frameworks and libraries. I have a keen interest in frontend technologies like React and Vue.
             Achieved a 1st class BSc focused in Computer Science from the University of Kent.
           </h5>
         </Container>
       </Jumbotron>
-      <div>
-
-      </div>
     </div>
   );
 }
