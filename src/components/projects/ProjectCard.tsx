aa7d436defbabdda435557e7b3bd8b8597c42269
OllieRumbol <otgb97@gmail.com>
2021-07-08 22:06:51 +0100

    article 3 update

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -17,15 +17,15 @@ const ProjectCard = (props: ProjectCardProps) => {
             <Card.Header className='display-5 text-center'>{props.title}</Card.Header>
             <Card.Body>
                 <Card.Text>
-                    <div className="row row-cols-1 row-col-sm-3">
-                        <div className='col-sm-7'>
+                    <div className="row row-cols-1 row-col-lg-3">
+                        <div className='col-lg-7'>
                             <h5 className='display-6'>Description</h5>
                             <p className='fs-4'>{props.description}</p>
                             <h5 className='display-6'>Technology</h5>
                             <p className='fs-4'>{props.technology}</p>
                             <a href={props.site} target="_blank" rel="noreferrer" className='btn btn-success mb-3'>{props.buttonText}</a>
                         </div>
-                        <div className='col-sm-5 d-flex justify-content-center'>
+                        <div className='col-lg-5 d-flex justify-content-center'>
                             <img className='img-fluid' src={props.image} alt={props.title} />
                         </div>
                     </div>
