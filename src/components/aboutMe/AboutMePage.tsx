443031eb534d1e2e2678cd9c413892557449d472
Oliver Bourne <otgb97@gmail.com>
2022-11-14 22:52:03 +0000

    refineing PageSuite experience

--- src/components/aboutMe/AboutMePage.tsx
+++ src/components/aboutMe/AboutMePage.tsx
@@ -1,78 +1,142 @@
-import Container from 'react-bootstrap/Container';
-import '../../style/Shared.css';
+import Container from "react-bootstrap/Container";
+import "../../style/Shared.css";
 
 const AboutMePage = () => {
-
   return (
-    <Container fluid className='p-4'>
-      <h1 className='text-center colour display-1'>About Me</h1>
-      <h4 className='colour display-4 text-success'>Technical Experience</h4>
-      <h6 className='colour display-6'>June 2020 – Now: Technical Lead at PageSuite</h6>
+    <Container fluid className="p-4">
+      <h1 className="text-center colour display-1">About Me</h1>
+      <h4 className="colour display-4 text-success">Technical Experience</h4>
+      <h6 className="colour display-6">
+        June 2020 – Now: Backend Team Lead at PageSuite
+      </h6>
       <ul>
         <li>
-          <span className='colour fs-4'>
-            Backend team: Backend team: In the middle of 2021 PageSuite reformed the backend team. At the end of 2021, I was promoted 
-            from backend developer to technical lead. In the back half of 2021, my job was slightly different. I was still coding and 
-            building new products but I went on more sales calls, more scoping meetings and more planning meetings. As a more 
-            experienced developer, I created more generic solutions instead of just building what was needed. The best of which was 
-            the generic SSO gateway that allowed third parties or custom SSO solutions to integrate with all products at PageSuite. 
-            This new SSO solution enabled developers to integrate with nearly any SSO in hours instead of days. Another replaceability 
-            I picked up over the year was that when the team lead of backend and infrastructure was on holiday or off ill, I had the 
-            honour of leading the backend team.
+          <span className="colour fs-4">
+            PageSuite is a digital publishing company working with publishers
+            all over the world. We have a variety of app and web based products
+            used in the industry. During my time there I used many different
+            technologies from C#, .NET (.NET 6, Core, FrameWork) to AWS and
+            TypeScript.
           </span>
         </li>
         <li>
-          <span className='colour fs-4'>
-            Client team: Joining the client team was a big mix up in the company. We focused on the custom work within the company so,
-            we created custom modals, managed IAPs, login and payment integrations, Oauth gateway pages, JSON and XML automations along
-            with one off projects for clients all over the world. As a backend developer, we were also involved with project management,
-            client calls, bug master and on call support. At PageSuite we used C# with .NET 5, .NET Core, .NET Framework, JS with Node
-            and AWS infrastructure.
-          </span>
-        </li>
-        <li>
-          <span className='colour fs-4'>
-            Backend team: When I first joined the company teams were split up into technologies. Backend developers were the closes
-            thing PageSuite had to a full stack developers. We built automation's, integrations, feed wrappers anything the company needed
-            for the apps and frontend to work the client data for digital publishing. PageSuite can sometimes be as agile as a waterfall, which
-            is something im trying to always improve in the company.
+          <span className="colour fs-4">
+            I have been involved in a few different teams at PageSuite but my
+            work has always been very backend focused. I have also spent a large
+            amount of time taking on responsibilities of the job position above
+            mine.
+            <ul>
+              <li>
+                <span className="colour fs-4">
+                  <b>Backend Team Lead:</b> It was time for me to take a step
+                  back from coding and provide some fresh leadership to the
+                  backend team after a long period of unstable leadership. This
+                  was the first time in my career I took on a leadership
+                  position, I thought it was important for me to learn from
+                  mistakes I had scene in the past and trust my gut. I wanted to
+                  support and nurture my team and help them grow as a group. I
+                  added a variety of new ideas to the team such as
+                  retrospectives, technical discussions, backlog refinements,
+                  one-to-ones, and creating more documentation about our
+                  products. After the success of the backend team, I took over
+                  more teams such as the platform team who dealt with the AWS
+                  infrastructure and the portal team which was developing a new
+                  management portal for the business.
+                </span>
+              </li>
+              <li>
+                <span className="colour fs-4">
+                  <b>Backend Technical Lead:</b> The biggest responsibility I
+                  had as a "Technical Lead" was to push our chosen technology in
+                  the right direction and get as much business value as
+                  possible. This allowed developers to start using the latest
+                  versions of .NET instead of always using the .NET Framework. I
+                  created more generic solutions instead of just building what
+                  was needed. The best of which was the generic SSO gateway that
+                  allowed third parties or custom SSO solutions to be integrated
+                  with our all products at PageSuite. This new SSO solution
+                  enabled developers to integrate with any SSO in hours instead
+                  of days.
+                </span>
+              </li>
+              <li>
+                <span className="colour fs-4">
+                  <b>Backend Developer:</b> Now that I was no longer a junior
+                  developer the biggest change was being more heavily involved
+                  in the sales process at PageSuite (as a junior I worked on a
+                  few SOWs and sales calls but not the whole process). I started
+                  to help lead the sales process from a technical point of view.
+                  Breaking down customer requirements, scoping estimations and
+                  feasibility, technical breakdown with major clients were just
+                  a few of the tasks. But by far, the best part of being a
+                  backend developer was having more weight in the
+                  decision-making of how we built new features and products.
+                  This allowed be to start pushing code quality within the
+                  business.
+                </span>
+              </li>
+              <li>
+                <span className="colour fs-4">
+                  <b>Junior Backend Developer:</b> Being my first developer job
+                  after university, I was excited to dive into the deep end and
+                  get my hands dirty with all sorts of projects. The first month
+                  or two was support tickets which gave me great experience in a
+                  variety of projects from management portals, subscription
+                  services and the HTML5 PDF Reader. This was quickly followed
+                  by actual backend development tasks such as feed wrappers,
+                  third-party API login integrations, web endpoints and
+                  automations. These experiences gave me a great base
+                  understanding of the product and services available at
+                  PageSuite.
+                </span>
+              </li>
+            </ul>
           </span>
         </li>
       </ul>
-      <h6 className='colour display-6'>Aug 2018 – Sep 2019: Software Developer at AXA PPP</h6>
+      <h6 className="colour display-6">
+        Aug 2018 – Sep 2019: Software Developer at AXA PPP
+      </h6>
       <ul>
         <li>
-          <span className='colour fs-4'>
-            PMA team: Was a tool used by the provider team to manage information with all types of providers. This could be a small
-            specialist or a major hospital. PMA was a huge 8 year project (4 years when I joined) and included a variety of tools
-            that were built using .NET MVC and jQuery.
+          <span className="colour fs-4">
+            PMA team: Was a tool used by the provider team to manage information
+            with all different types of providers. This could be a small
+            specialist or a major hospital. PMA was a huge 8 year project (4
+            years when I joined) and included a variety of tools that were built
+            using .NET MVC and jQuery.
           </span>
         </li>
         <li>
-          <span className='colour fs-4'>
-            PACE team: This was the first agile team I join at AXA PPP where I was one of two .NET developers so a lot was
-            expected of me. Pace was an in house tool for gathering marketing consent across the business. It had an Angular frontend
-            and a .NET API with a variety of automations and databases behind it. The IBM tool MDM played a huge part in the project.
-            I worked on both the frontend and backend of the application along with fully automated testing using MS Test, Specflow
-            and selenium.
+          <span className="colour fs-4">
+            PACE team: This was the first agile team I join at AXA PPP where I
+            was one of two .NET developers so a lot was expected of me. Pace was
+            an in house tool for gathering marketing consent across the
+            business. It had an Angular frontend and a .NET API with a variety
+            of automations and databases behind it. The IBM tool MDM played a
+            huge part in the project. I worked on both the frontend and backend
+            of the application along with fully automated testing using MS Test,
+            Specflow and selenium.
           </span>
         </li>
         <li>
-          <span className='colour fs-4'>
-            AXA PPP: Uses Azure and .NET as its main infrastructure and had 8 agile teams. I really enjoyed working in an open
-            plan office and learning from other developers.
+          <span className="colour fs-4">
+            AXA PPP: Uses Azure and .NET as its main infrastructure and had 8
+            agile teams. I really enjoyed working in an open plan office and
+            learning from other developers.
           </span>
         </li>
       </ul>
-      <hr className='green' />
-      <h4 className='colour display-4 text-success'>Skills</h4>
-      <p className='colour fs-4'>
-        I'm always excited to learn new libraries, frameworks, tools and languages to help improve my skills as a developer and create
-        the best projects I can. Below is a list of skills I have used so far.
+      <hr className="green" />
+      <h4 className="colour display-4 text-success">Skills</h4>
+      <p className="colour fs-4">
+        I'm always excited to learn new libraries, frameworks, tools and
+        languages to help improve my skills as a developer and create the best
+        projects I can. Below is a list of skills I have used so far.
       </p>
-      <div className='row row-cols-1 row-cols-md-4'>
-        <div className='col'>
-          <ul className='fs-4'>
+      <div className="row row-cols-1 row-cols-md-4">
+        <div className="col">
+          <ul className="fs-4">
             <li>
               <span>Languages</span>
               <ul>
@@ -107,8 +171,8 @@ const AboutMePage = () => {
             </li>
           </ul>
         </div>
-        <div className='col'>
-          <ul className='fs-4'>
+        <div className="col">
+          <ul className="fs-4">
             <li>
               <span>Libraries and frameworks</span>
               <ul>
@@ -116,7 +180,9 @@ const AboutMePage = () => {
                   <span>Angular, Vue, React</span>
                 </li>
                 <li>
-                  <span>.NET 5, .NET Core, .NET Framework (MVC and Web Forms)</span>
+                  <span>
+                    .NET 5, .NET Core, .NET Framework (MVC and Web Forms)
+                  </span>
                 </li>
                 <li>
                   <span>Bootstrap</span>
@@ -129,15 +195,16 @@ const AboutMePage = () => {
                 </li>
                 <li>
                   <span>
-                    Automated testing: MS Test, X-Unit, N-Unit, Selenium, SpecFlow
+                    Automated testing: MS Test, X-Unit, N-Unit, Selenium,
+                    SpecFlow
                   </span>
                 </li>
               </ul>
             </li>
           </ul>
         </div>
-        <div className='col'>
-          <ul className='fs-4'>
+        <div className="col">
+          <ul className="fs-4">
             <li>
               <span>Cloud Services/ Deployments</span>
               <ul>
@@ -157,8 +224,8 @@ const AboutMePage = () => {
             </li>
           </ul>
         </div>
-        <div className='col'>
-          <ul className='fs-4'>
+        <div className="col">
+          <ul className="fs-4">
             <li>
               <span>Tools</span>
               <ul>
@@ -194,53 +261,127 @@ const AboutMePage = () => {
           </ul>
         </div>
       </div>
-      <hr className='green' />
-      <h4 className='colour display-4 text-success'>Education</h4>
-      <h6 className='colour display-6'>2016 - 2020 1st Class BSc in Computer Science, University of Kent</h6>
-      <ul className='colour fs-4'>
-        <li className='fw-bolder'><span>Final year <span className="badge bg-success">73%</span></span></li>
+      <hr className="green" />
+      <h4 className="colour display-4 text-success">Education</h4>
+      <h6 className="colour display-6">
+        2016 - 2020 1st Class BSc in Computer Science, University of Kent
+      </h6>
+      <ul className="colour fs-4">
+        <li className="fw-bolder">
+          <span>
+            Final year <span className="badge bg-success">73%</span>
+          </span>
+        </li>
         <ul>
-          <li><span>Introduction to Intelligent Systems</span></li>
-          <li><span>Computing Law and Professional Responsibility</span></li>
-          <li><span>Computing in the Classroom</span></li>
-          <li><span>IT Consultancy Project (final year project)</span></li>
-          <li><span>Internet of Things</span></li>
-          <li><span>Programming Language Implementation</span></li>
-          <li><span>Theory and Practice of Concurrency</span></li>
+          <li>
+            <span>Introduction to Intelligent Systems</span>
+          </li>
+          <li>
+            <span>Computing Law and Professional Responsibility</span>
+          </li>
+          <li>
+            <span>Computing in the Classroom</span>
+          </li>
+          <li>
+            <span>IT Consultancy Project (final year project)</span>
+          </li>
+          <li>
+            <span>Internet of Things</span>
+          </li>
+          <li>
+            <span>Programming Language Implementation</span>
+          </li>
+          <li>
+            <span>Theory and Practice of Concurrency</span>
+          </li>
         </ul>
-        <li className='fw-bolder'><span>Industry year <span className="badge bg-success">81%</span></span></li>
+        <li className="fw-bolder">
+          <span>
+            Industry year <span className="badge bg-success">81%</span>
+          </span>
+        </li>
         <ul>
-          <li><span>Software developer at AXA PPP</span></li>
+          <li>
+            <span>Software developer at AXA PPP</span>
+          </li>
         </ul>
-        <li className='fw-bolder'><span>Second year <span className="badge bg-success">74%</span></span></li>
+        <li className="fw-bolder">
+          <span>
+            Second year <span className="badge bg-success">74%</span>
+          </span>
+        </li>
         <ul>
-          <li><span>Software Engineering</span></li>
-          <li><span>Algorithms, Correctness and Efficiency</span></li>
-          <li><span>Theory of Computing</span></li>
-          <li><span>Operating Systems and Architecture</span></li>
-          <li><span>Database Systems</span></li>
-          <li><span>Web Development</span></li>
-          <li><span>Functional and Concurrent Programming</span></li>
+          <li>
+            <span>Software Engineering</span>
+          </li>
+          <li>
+            <span>Algorithms, Correctness and Efficiency</span>
+          </li>
+          <li>
+            <span>Theory of Computing</span>
+          </li>
+          <li>
+            <span>Operating Systems and Architecture</span>
+          </li>
+          <li>
+            <span>Database Systems</span>
+          </li>
+          <li>
+            <span>Web Development</span>
+          </li>
+          <li>
+            <span>Functional and Concurrent Programming</span>
+          </li>
         </ul>
-        <li className='fw-bolder'><span>First year <span className="badge bg-success">72%</span></span></li>
+        <li className="fw-bolder">
+          <span>
+            First year <span className="badge bg-success">72%</span>
+          </span>
+        </li>
         <ul>
-          <li><span>Introduction to Object-Oriented Programming</span></li>
-          <li><span>Foundations of Computing 1</span></li>
-          <li><span>Databases and the Web</span></li>
-          <li><span>Computer Systems</span></li>
-          <li><span>Foundations of Computing 2</span></li>
-          <li><span>Human Computer Interaction</span></li>
-          <li><span>People and Computing</span></li>
-          <li><span>Further Object-Oriented Programming</span></li>
+          <li>
+            <span>Introduction to Object-Oriented Programming</span>
+          </li>
+          <li>
+            <span>Foundations of Computing 1</span>
+          </li>
+          <li>
+            <span>Databases and the Web</span>
+          </li>
+          <li>
+            <span>Computer Systems</span>
+          </li>
+          <li>
+            <span>Foundations of Computing 2</span>
+          </li>
+          <li>
+            <span>Human Computer Interaction</span>
+          </li>
+          <li>
+            <span>People and Computing</span>
+          </li>
+          <li>
+            <span>Further Object-Oriented Programming</span>
+          </li>
         </ul>
       </ul>
-      <h6 className='colour display-6'>2009 – 2016 Sixth Form and Secondary School, John Wallis Academy, Ashford</h6>
-      <ul className='colour fs-4'>
-        <li><span>3 A Levels: Computing (C), Geography (C), IT BTEC (Double Distinction Star)</span></li>
-        <li><span>12 GCSEs and 2 BTECs from A* - C</span></li>
+      <h6 className="colour display-6">
+        2009 – 2016 Sixth Form and Secondary School, John Wallis Academy,
+        Ashford
+      </h6>
+      <ul className="colour fs-4">
+        <li>
+          <span>
+            3 A Levels: Computing (C), Geography (C), IT BTEC (Double
+            Distinction Star)
+          </span>
+        </li>
+        <li>
+          <span>12 GCSEs and 2 BTECs from A* - C</span>
+        </li>
       </ul>
     </Container>
   );
-}
+};
 
 export default AboutMePage;
--- src/components/projects/ProjectsPage.tsx
+++ src/components/projects/ProjectsPage.tsx
@@ -1,60 +1,80 @@
-import ProjectCard from './ProjectCard';
-import Container from 'react-bootstrap/Container';
-import '../../style/Shared.css';
-import DraughtsGameImage from '../../assets/DraughtsGame.PNG';
-import Modal from '../../assets/Modal.png';
-import TreeCalculator from '../../assets/TreeCalculator.PNG';
+import ProjectCard from "./ProjectCard";
+import Container from "react-bootstrap/Container";
+import "../../style/Shared.css";
+import DraughtsGameImage from "../../assets/DraughtsGame.PNG";
+import Modal from "../../assets/Modal.png";
+import TreeCalculator from "../../assets/TreeCalculator.PNG";
 
 const ProjectPage = () => {
-
   return (
-    <Container fluid className='p-4'>
-      <h1 className='text-center p-1 colour display-1'>Projects</h1>
-      <p className='colour fs-4'>
-        Over the years I have built many projects to help develop my skills as a developer using all sorts of technology, libraries, frameworks and tools.
+    <Container fluid className="p-4">
+      <h1 className="text-center p-1 colour display-1">Projects</h1>
+      <p className="colour fs-4">
+        Over the years I have built many projects to help develop my skills as a
+        developer using all sorts of technology, libraries, frameworks and
+        tools.
       </p>
       <ProjectCard
-        title='Draughts Game'
-        description='A draughts game built that offers players both two-player and single-player modes with a range of difficulty levels.
-          The game has many features to help players during the game, such as displaying all potential moves, whos turn it is, undo and the 
-          number of pieces taken for each player.'
-        technology='The front end of the game is built using react js and many other react libraries. Whilst the backend uses Azure 
-          serverless functions combined with the minimax algorithm to calculate the PCs next move. The site has been deployed using 
-          GitHub pages with a YAML pipeline to automate deployments.'
-        site='https://ollierumbol.github.io/draughtsgame/#/'
-        image={DraughtsGameImage}
-        buttonText='Link to site' />
-        <br></br>
+        title="JWT Validator"
+        description="The goal of this library is to simplify the validation of a JWT in C# and abstract away from the complex internal libraries 
+        I developed this library becasue of dealing with the reocurring issue of developers strugging with the validation of an ID token after a 
+        sucessful OAuth login. There are three main parts to this library. 1. A builder to constrct what parts of the JWT need validating. 2. 
+        Methods that return whether the JWT is valid or not as well as a dictionary of what JWT contains. 3. Dictionary extension methods to 
+        help handle the results."
+        technology="This was my first project I release as a nuget library and shared with other developers. Its built using C# and .NET 6. 
+        The are a varity of different aspect to this project, builder design pattern, try get methods, generic extension methods and unit tests."
+        site="https://www.nuget.org/packages/JWTValidator"
+        image={""}
+        buttonText="Link to library"
+      />
+      <br></br>
       <ProjectCard
-        title='Dynamic Vue Modal'
-        description='The idea of this project was to mix and match old and new technologies. Companies sometimes have so much technical 
+        title="Draughts Game"
+        description="A draughts game built that offers players both two-player and single-player modes with a range of difficulty levels.
+          The game has many features to help players during the game, such as displaying all potential moves, whos turn it is, undo and the 
+          number of pieces taken for each player."
+        technology="The front end of the game is built using react js and many other react libraries. Whilst the backend uses Azure 
+          serverless functions combined with the minimax algorithm to calculate the PCs next move. The site has been deployed using 
+          GitHub pages with a YAML pipeline to automate deployments."
+        site="https://ollierumbol.github.io/draughtsgame/#/"
+        image={DraughtsGameImage}
+        buttonText="Link to site"
+      />
+      <br></br>
+      <ProjectCard
+        title="Dynamic Vue Modal"
+        description="The idea of this project was to mix and match old and new technologies. Companies sometimes have so much technical 
           debt that they believe they have no other choice but to carry on using old technologies in new projects and make the 
           problem worse. This solution dynamically loads Vue and other dependencies without interfering with old technology. 
-          The modal allows users to log in.'
-        technology='The project uses as much vanilla js as possible and relies on Vue for DOM manipulation. This means when the next 
-        best frontend framework comes out when can use it as quickly as possible. It can be configured using a JSON file on AWS S3.'
-        site='https://github.com/OllieRumbol/Dynamic-Vue'
+          The modal allows users to log in."
+        technology="The project uses as much vanilla js as possible and relies on Vue for DOM manipulation. This means when the next 
+        best frontend framework comes out when can use it as quickly as possible. It can be configured using a JSON file on AWS S3."
+        site="https://github.com/OllieRumbol/Dynamic-Vue"
         image={Modal}
-        buttonText='Link to repo' />
-        <br></br>
+        buttonText="Link to repo"
+      />
+      <br></br>
       <ProjectCard
-        title='Tree Calculator'
-        description='This program solves calculations by creating a tree from an equation and then solving a tree. It follows the 
+        title="Tree Calculator"
+        description="This program solves calculations by creating a tree from an equation and then solving a tree. It follows the 
           BIDMAS logic (Brackets, Indices, Division, Multiplication, Addition, Subtraction) and can deal with all sort of 
           calculations +, -, *, /, !, √, powers and brackets. Validation does occur before creating a tree for example dividing 
-          by 0 or missing out numbers.'
-        technology='A .NET Core console application has been used with .NET standard class libraries and MS unit tests.'
-        site='https://github.com/OllieRumbol/TreeCalculator'
+          by 0 or missing out numbers."
+        technology="A .NET Core console application has been used with .NET standard class libraries and MS unit tests."
+        site="https://github.com/OllieRumbol/TreeCalculator"
         image={TreeCalculator}
-        buttonText='Link to repo' />
+        buttonText="Link to repo"
+      />
       <br></br>
-      <p className='colour fs-4'>
-        After my first year of university, I built all sorts of applications in Java to help develop my foundation knowledge as a
-        developer. Such as clocks, calculators, text editors, track archery scoring, track table tennis matches, encoding and
-        decoding messages, binary converters, code breakers and traffic light systems.
+      <p className="colour fs-4">
+        After my first year of university, I built all sorts of applications in
+        Java to help develop my foundation knowledge as a developer. Such as
+        clocks, calculators, text editors, track archery scoring, track table
+        tennis matches, encoding and decoding messages, binary converters, code
+        breakers and traffic light systems.
       </p>
     </Container>
   );
-}
+};
 
 export default ProjectPage;
